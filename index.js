2de99ba7447e3be389302e521f77b2202def8914
Matthew K <arcmatthewrk@gmail.com>
2025-08-01 17:26:01 +0200

    Add view_post.ejs. Refactor post request to display view post form instead of home page. Add loop on home page to display all post titles. Add article counter to home page GET request.

--- index.js
+++ index.js
@@ -9,17 +9,19 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 /* HTTP request routing */
-
+/*delete post form, do as button on view article page*/
 app.delete("/delete", (req, res) => {
     console.log('got a delete request')
     /*res.render("delete_post_form.ejs") */
 });
 
+/* display edit post form, Edit post, save, and display edited post pagedo as button on view article page*/
 app.patch("/edit", (req, res) => {
     console.log('got a patch request')
     /*res.render("edit_post_form.ejs") */
 });
 
+/*New post submission, save, and display Post page*/
 app.post("/create_post", (req, res) => {
     console.log("post request form data JS object: ", req.body);
 
@@ -37,33 +39,47 @@ app.post("/create_post", (req, res) => {
             console.log("posts.json updated")
         })
     });
-    
-    res.render("index.ejs", req.body); 
+
+    res.render("view_post.ejs", req.body);
 });
 
+/*Display create post form*/
 app.get("/new_post_form", (req, res) => {
     console.log("Display create post form");
     res.render("new_post_form.ejs")
 });
 
+/*Display Home page*/
 app.get("/", (req, res) => {
     console.log("Display homepage");
    
     fs.readFile("./data/posts.json", "utf-8", (err, data) => {
         if (err) throw (err);
         const jsObjectArray = JSON.parse(data);
-        let arrayLength = [jsObjectArray.length];
-        let latestPost = jsObjectArray[arrayLength - 1];
+        const arrayLength = [jsObjectArray.length];
+        const latestPost = jsObjectArray[arrayLength - 1];
 
-        /*console.log("read .json: ", data); 
+        const posts = {
+            title: latestPost.title,
+            body: latestPost.body,
+            author: latestPost.author,
+            quantity: arrayLength,
+            recentPosts: jsObjectArray
+        };
+
+        /*
+        console.log("read .json: ", data); 
         console.log("array length: ", arrayLength);       
         console.log(".json parsed: ", jsObjectArray);
-        console.log("latest Post: ", latestPost);*/
+        console.log("latest Post: ", latestPost);
+        console.log("posts: ", posts);
+        */
 
-        res.render("index.ejs", latestPost)
+        res.render("index.ejs", posts)
     });
-})
+});
 
+/*Server connection*/
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 });
--- middleware/validatePost.js
+++ middleware/validatePost.js
@@ -1,31 +1,4 @@
 /* experiments */
-const posts = [
-    {article_name: "yippjie", article_body: "curvy", author_name: "mayy"}, 
-    {title: "Is your dog a frog?", body: "If it ribbets instead of barks, then your dog might be a frog!", author: "Toadd"},
-    {title: "Is your Bog a Mog?", body: "If it's a purple Tuesday, then your Bog might be a Mog!", author: "Bojack"}
-];
-/*
-console.log("global1:", posts[0]);
-console.log("global2:", posts[1]);
-console.log("global3:", posts[1].title); all 3 indexing methods work, so potentially use to filter search*/
-
-
-
-/*
-const obj = {name: "jackson", username: "cheeckychipmunks", color: "blue"};
-const myJSON = JSON.stringify(obj);  
-const myJS = JSON.parse(myJSON);
-
-console.log(obj.name);
-console.log(myJSON.color);cant access JSON data directly, must turn into JS object first using .parse
-console.log(myJS.username);
-
-fs.readFile("./data/posts.json", "utf-8", (err, data) => {
-    if (err) throw (err);
-    console.log(data)/*the data is the contents of the file to can use this to get and send to ejs 
-})
-*/
-
 /* 
 custom middleware notation
 function myMiddleware(req, res, next) {
@@ -37,16 +10,18 @@ to use middleware
 app.use(myMiddleware); 
 */
 
-function saveAsJSON (req, res, next) {
-    const obJS = JSON.stringify(req.body);
-    console.log("JS object stringify: ", obJS);
+const posts = [
+    {title: "yippjie", body: "curvy", author: "mayy"}, 
+    {title: "Is your dog a frog?", body: "If it ribbets instead of barks, then your dog might be a frog!", author: "Toadd"},
+    {title: "Is your Bog a Mog?", body: "If it's a purple Tuesday, then your Bog might be a Mog!", author: "Bojack"}
+];
 
-    fs.writeFile("./data/posts.json", obJS, (err) => { 
-        if (err) throw (err);
-        console.log("posts.json updated")
-    });    
-    next();
-}
+/*
+const myJSON = JSON.stringify(posts);  JS object to string object literal
+const myJS = JSON.parse(myJSON);       JSON string object literal to JS object 
 
-app.use(saveAsJSON); 
-
+console.log(posts[0]);   must provide index of array to access property, if one item in array then can access property directly (posts.title)
+console.log(posts[1].title);
+console.log(myJSON[2].title);cant access JSON data directly, must turn into JS object first using .parse
+console.log(myJS.[0].title);
+*/
